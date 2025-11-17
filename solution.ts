c5324be6fbad9ca354792d43904442b6e7ba0abe
Md.Tanvir <hasanmdtanvir916@gmail.com>
2025-11-17 14:52:12 +0600

    submit six of solutions

--- solution.ts
+++ solution.ts
@@ -8,9 +8,6 @@ function formatValue(
   } else return !value;
 }
 
-
-
-
 function getLength(value: string | any[]): number {
   if (Array.isArray(value)) {
     return value.length;
@@ -18,9 +15,6 @@ function getLength(value: string | any[]): number {
     return value.length;
   }
 }
-
-
-
 
 class Person {
   name: string;
@@ -35,4 +29,44 @@ class Person {
   }
 }
 
+type item = {
+  title: string;
+  rating: number;
+};
 
+function filterByRating(items: item[]): item[] {
+  for (const item of items) {
+    if (item.rating < 0 || item.rating > 5) {
+      throw new Error("Rating must be between 0 to 5");
+    }
+  }
+
+  return items.filter((item) => item.rating >= 4);
+}
+
+type user = {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function filterActiveUsers(users: user[]): user[] {
+  return users.filter((user) => user.isActive === true);
+}
+
+
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): void {
+  const available = book.isAvailable ? "Yes" : "No";
+  console.log(
+    `Title : ${book.title}, Author : ${book.author}, Published : ${book.publishedYear},  Available : ${available}`
+  );
+}
